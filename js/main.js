a571876a45ae5cdbd1ec323e458a5925fafb3c12
mfox05 <maciejfoks22@gmail.com>
2023-06-04 20:22:31 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,51 +1,13 @@
-const converter = document.querySelector('#converter')
-const result = document.querySelector('.result')
-const convBtn = document.querySelector('.conv')
-const resetBtn = document.querySelector('.reset')
-const changeBtn = document.querySelector('.change')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
+const burgerBtn = document.querySelector('.burger')
+const barsIco = document.querySelector('.fa-bars')
+const xIco = document.querySelector('.fa-times')
+const nav = document.querySelector('nav ul')
 
-let fahrenheit
-let celsius
-
-const swap = () => {
-	if (one.textContent == '°C') {
-		one.textContent = '°F'
-		two.textContent = '°C'
-        result.textContent = ''
-	} else {
-        two.textContent = '°F'
-		one.textContent = '°C'
-        result.textContent = ''
-	}
-}
-const fahrToCel = () => {
-    fahrenheit = converter.value * 1.8 + 32
-	result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
-	converter.value = ''
-}
-const celToFahr = () => {
-    celsius = (converter.value - 32) / 1.8
-    result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
-    converter.value = ''
-}
-const conversion = () => {
-	if (converter.value !== '') {
-        if (one.textContent === '°C'){
-            fahrToCel()
-        } else {
-            celToFahr()
-        }
-	} else {
-        result.textContent = 'Musisz podać jakąś wartość'
-	}
-}
-const reset = () => {
-    converter.value = ''
-    result.textContent = ''
+const handleNav = () => {
+    nav.classList.toggle('active')
+    burgerBtn.classList.toggle('active')
+    barsIco.classList.toggle('hide')
+    xIco.classList.toggle('hide')
 }
 
-changeBtn.addEventListener('click', swap)
-convBtn.addEventListener('click', conversion)
-resetBtn.addEventListener('click', reset)
+burgerBtn.addEventListener('click', handleNav)
